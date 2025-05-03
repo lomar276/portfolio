7eec38a49cb9e89e95bdb7b5e1564b4a8e1fc20a
lomar276 <louis.martineau.2@ulaval.ca>
2025-05-02 21:10:58 -0400

    Add TanStack Router

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,21 @@
+import { Link } from "@tanstack/react-router";
+
 const pages = [
-  { href: "/", text: "Accueil" },
-  { href: "/about", text: "À propos" },
+  { to: "/", text: "Accueil" },
+  { to: "/about", text: "À propos" },
 ];
-const currentPath = window.location.pathname;
 
 export default function Navbar() {
   return (
     <ul className="absolute flex justify-center items-center w-full h-20 top-0 left-0 space-x-4">
-      {pages.map(({ href, text }) => (
-        <li className="text-xl relative" key={href}>
-          <a
-            className={`hover:text-secondary-hover-500 hover:mb-5 duration-300 ${
-              currentPath === href
-                ? "underline-offset-8 decoration-secondary-hover-500"
-                : "no-underline"
-            }`}
-            href={href}
+      {pages.map(({ to, text }) => (
+        <li className="text-xl relative" key={to}>
+          <Link
+            to={to}
+            className="hover:text-secondary-hover-500 hover:mb-5 duration-300 [&.active]:underline [&.active]:underline-offset-8 [&.active]:decoration-secondary-hover-500"
           >
             {text}
-          </a>
+          </Link>
         </li>
       ))}
     </ul>
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
 import { createRoot } from "react-dom/client";
+import { routeTree } from "./routeTree.gen";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import "./index.css";
-import App from "./App.tsx";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 const rootElement = document.getElementById("root")!;
-
-createRoot(rootElement).render(<App />);
+createRoot(rootElement).render(<RouterProvider router={router} />);
