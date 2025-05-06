7e378024ff4bdc3310b0f1bb6b2962ae5c479cd8
lomar276 <louis.martineau.2@ulaval.ca>
2025-05-06 13:29:52 -0400

    better Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,10 +6,10 @@ interface CardProps {
 
 export default function Card({ title, description, logo }: CardProps) {
   return (
-    <div className="bg-gray-800/[.75] hover:bg-slate-700/[.75] shadow-sm hover:shadow-xl hover:cursor-pointer rounded-lg p-3 hover:scale-105 duration-500">
-      <div className="justify-center items-center flex flex-col space-y-1">
-        {logo}
+    <div className="bg-gray-800/[.75] hover:bg-slate-700/[.75] shadow-sm hover:shadow-xl hover:cursor-pointer rounded-lg p-3 hover:scale-105 duration-500 flex flex-col">
+      <div className="flex-1 flex justify-center items-center mb-2">{logo}</div>
 
+      <div className="flex-1 flex flex-col justify-center items-center">
         <div className="text-xl text-slate-50">{title}</div>
         {description && (
           <div className="text-md text-slate-200">{description}</div>
