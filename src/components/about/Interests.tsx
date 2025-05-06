026a94eb8f8be392e5748aca02ef6bcb4003965a
lomar276 <louis.martineau.2@ulaval.ca>
2025-05-06 12:23:33 -0400

    using lucide icons for Interests

--- src/components/about/Interests.tsx
+++ src/components/about/Interests.tsx
@@ -1,38 +1,32 @@
-import {
-  faCode,
-  faFilm,
-  faTableTennisPaddleBall,
-  faDiceSix,
-} from "@fortawesome/free-solid-svg-icons";
-import sigma from "../../assets/svgs/interests/sigma.svg";
-import Card from "../Card";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Sigma, Code, Clapperboard, Dice6, Icon } from "lucide-react";
+import { batBall } from "@lucide/lab";
 import Section from "./Section";
+import Card from "../Card";
 
 const interests = [
   {
     name: "Mathématiques",
-    logo: sigma,
+    logo: Sigma,
     description: "Je me passionne pour les maths",
   },
   {
     name: "Programmation",
-    logo: faCode,
+    logo: Code,
     description: "Je me passionne pour la programmation, notamment fullstack",
   },
   {
     name: "Cinéma (montage vidéo)",
-    logo: faFilm,
+    logo: Clapperboard,
     description: "Je me passionne pour le cinéma et le montage vidéo",
   },
   {
     name: "Tennis, Ping-pong et Vélo",
-    logo: faTableTennisPaddleBall,
+    logo: batBall,
     description: "!",
   },
   {
     name: "Jeux de société",
-    logo: faDiceSix,
+    logo: Dice6,
     description: "!",
   },
 ];
@@ -41,18 +35,19 @@ export default function Interests() {
   return (
     <Section title="Mes intérêts">
       <div className="grid grid-cols-3 gap-4">
-        {interests.map(({ name, description, logo }) => (
+        {interests.map(({ name, description, logo: Logo }) => (
           <Card
             key={name}
             title={name}
             description={description}
             logo={
-              typeof logo === "string" ? undefined : ( // TODO
-                <FontAwesomeIcon
-                  icon={logo}
-                  size="2x"
+              Array.isArray(Logo) ? (
+                <Icon
+                  iconNode={Logo}
                   className="w-16 h-16 mx-auto text-slate-50"
                 />
+              ) : (
+                <Logo className="w-16 h-16 mx-auto text-slate-50" />
               )
             }
           />
